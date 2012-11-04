62598784ef51fb43e9ce7068d8f1ef95798a3690
Jeff Barczewski <jeff.barczewski@gmail.com>
2012-11-04 11:01:47 -0600

    add digesting for streams, with fixture to make sure that it matches that of unix shasum

--- test/digest.mocha.js
+++ test/digest.mocha.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var chai = require('chai-stack');
+var fs = require('fs');
+var path = require('path');
 var digest = require('../lib/digest');
 var t = chai.assert;
 
@@ -13,4 +15,32 @@ test('digest(data) returns digest', function () {
   t.equal(d1, d2, 'same str should have same digest');
   var d3 = digest(str + 'z');
   t.notEqual(d1, d3, 'different str should have diff digest');
+});
+
+test('digest() returns digester', function () {
+  var digester = digest();
+  t.isFunction(digester.update);
+  t.isFunction(digester.digest);
+});
+
+test('digester.update(data) and digester.digest(type) matches shasum unix command', function (done) {
+  var expectedSha1 = '648a6a6ffffdaa0badb23b8baf90b6168dd16b3a';
+  var digester = digest();
+  var inStream = fs.createReadStream(path.join(__dirname, 'fixtures/hello.txt'));
+  inStream.on('error', function (err) { done(err); });
+  inStream.on('data', function (data) { digester.update(data); });
+  inStream.on('end', function () {
+    var dig = digester.digest('hex');
+    t.equal(dig, expectedSha1, 'digest should match shasum unix calc');
+    done();
+  });
+});
+
+test('digest(data, "hex") matches that of shasum unix command', function (done) {
+  var expectedSha1 = '648a6a6ffffdaa0badb23b8baf90b6168dd16b3a';
+  fs.readFile(path.join(__dirname, 'fixtures/hello.txt'), function (err, data) {
+    var dig = digest(data, 'hex');
+    t.equal(dig, expectedSha1, 'digest should match shasum unix cal');
+    done();
+  });
 });
