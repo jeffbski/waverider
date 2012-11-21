370a5498ae1766ca3882bd8d149fd57b57d27709
Jeff Barczewski <jeff.barczewski@gmail.com>
2012-11-20 18:59:53 -0600

    continue implementation of gzip

--- test/content-mgr.mocha.js
+++ test/content-mgr.mocha.js
@@ -7,8 +7,21 @@ var cm = require('../lib/content-mgr');
 var digest = require('../lib/digest');
 var crypto = require('crypto');
 var zlib = require('zlib');
+var through = require('through'); // through streams
 
 var t = chai.assert;
+
+function ensureDataIsBuffer(data) {
+  /*jshint validthis:true */
+  this.queue((Buffer.isBuffer(data)) ? data : new Buffer(data));
+}
+
+function createBufferThroughStream() {
+  return through(ensureDataIsBuffer, function () {
+    console.warn('queuing a null');
+    this.queue(null); // queuing an end
+  });
+}
 
 suite('content-mgr');
 
@@ -75,28 +88,48 @@ test('cm.set(key, data, type, metaGzip) gzips and content, cm.getData(key, cb) r
 });
 
 test('cm.set(key, stream, type) saves stream, cm.getData(key, cb) retrieves data alone', function (done) {
-  var meta = { preprocess: ['gzip'] };
-  var origDataArr = ["<html><body><div>one", "</div><div>two</div><d", "iv>one</div><div>two</di", "v></body></html>"];
-  var wstream = new Stream();
-  var rstream = new MemoryStream();
-  wstream.pipe(rstream);
+  var origDataArr = ["Hello ", "World", " Goodbye ", "World"];
+  var rwStream = createBufferThroughStream();
   setTimeout(function () {
-    origDataArr.forEach(function (x) { wstream.emit('data', x); });
-    wstream.emit('end');
+    origDataArr.forEach(function (x) { rwStream.write(x); });
+    rwStream.end();
   }, 0);
-  var contentType = 'text/html';
-  cm.set(KEY, rstream, contentType, meta, function (err, result) {
+  var contentType = 'text/plain';
+  cm.set(KEY, rwStream, contentType, function (err, result) {
     t.isNull(err);
     cm.getData(KEY, function (err, data) {
       t.isNull(err);
-      zlib.gzip(origDataArr.join(''), function (err, expectedGzipData) {
+      var origData = origDataArr.join('');
+      t.equal(data, origData);
+      cm.getMeta(KEY, function (err, meta) {
+        var exptectedDigest = digest(origData);
+        t.equal(meta.digest, exptectedDigest);
+        done();
+      });
+    });
+  });
+});
+
+
+test('cm.set(key, stream, type, metaGzip) compresses and saves stream, cm.getData(key, cb) retrieves data alone', function (done) {
+  var meta = { preprocess: ['gzip'] };
+  var origDataArr = ["<html><body><div>one", "</div><div>two</div><d", "iv>one</div><div>two</di", "v></body></html>"];
+  var origData = origDataArr.join('');
+  console.warn('origDataLen', origData.length);
+  var rwStream = createBufferThroughStream();
+  setTimeout(function () {
+    origDataArr.forEach(function (x) { rwStream.write(x); });
+    rwStream.end();
+  }, 0);
+  var contentType = 'text/html';
+  cm.set(KEY, rwStream, contentType, meta, function (err, result) {
+    t.isNull(err);
+    cm.getData(KEY, function (err, data) {
+      t.isNull(err);
+      zlib.gunzip(data, function (err, unzippedData) {
         t.isNull(err);
-        var expectedGzipDigest = digest(expectedGzipData);
-        t.equal(digest(data), expectedGzipDigest);
-        cm.getMeta(KEY, function (err, meta) {
-          t.equal(meta.digest, expectedGzipDigest);
-          done();
-        });
+        t.equal(unzippedData.length, origData.length);
+        t.equal(digest(unzippedData), digest(origData));
       });
     });
   });
@@ -174,30 +207,127 @@ test('cm.getDataStream(key) returns a stream to the content', function (done) {
   });
 });
 
+test('through stream ends', function (done) {
+  var ReadableStream = require('../lib/readable');
+  var CHUNK_SIZE = 64 * 1024; // 64KB
+  var DATA_LENGTH = 2 * 1024 * 1024 + 25; // 2,025 KB
+  var rwStream = createBufferThroughStream();
+  rwStream.on('end', function () { console.warn('rwStream received end'); });
+  var bytesToGenerate = DATA_LENGTH;
+  function gen() {
+    var size = (bytesToGenerate > CHUNK_SIZE) ? CHUNK_SIZE : bytesToGenerate;
+    var buff = crypto.randomBytes(size);
+    rwStream.write(buff);
+    bytesToGenerate -= size;
+    if (!bytesToGenerate) {
+      rwStream.end();
+      console.warn('end was sent');
+      return;
+    }
+    process.nextTick(gen);
+  }
+  process.nextTick(gen);
+  var readable = new ReadableStream();
+  readable.wrap(rwStream);
+  var ended = false;
+  readable.on('end', function () { ended = true; console.warn('end received'); flow(); });
+  // readable.on('end', flow);
+  function flow(err) {
+    if (err) return done(err);
+    var chunk = readable.read();
+    if (!chunk) {
+      console.warn('flow empty read, ended:', ended);
+      if (ended) return handleNoDataEnded();
+      return readable.once('readable', flow);
+    }
+    setTimeout(function () { // slow handling of data chunk
+      console.warn('data len', chunk.length);
+      process.nextTick(flow);
+    }, 100);
+  }
+  var endHandled = false;
+  function handleNoDataEnded() {
+    if (!endHandled) {
+      endHandled = true;
+      console.warn('in handle end');
+      done();
+    }
+  }
+  flow();
+  // rwStream
+  //   .on('error', function (err) { done(err); })
+  //   .on('data', function (data) { console.warn('data len', data.length); })
+  //   .on('end', function () { console.warn('end received'); done(); });
+});
+
+test('simple through stream', function (done) {
+  var CHUNK_SIZE = 64 * 1024; // 64KB
+  var DATA_LENGTH = 2 * 1024 * 1024 + 25; // 2,025 KB
+  var rwStream = createBufferThroughStream();
+  var bytesToGenerate = DATA_LENGTH;
+  function gen() {
+    var size = (bytesToGenerate > CHUNK_SIZE) ? CHUNK_SIZE : bytesToGenerate;
+    var buff = crypto.randomBytes(size);
+    rwStream.write(buff);
+    bytesToGenerate -= size;
+    if (!bytesToGenerate) {
+      rwStream.end();
+      console.warn('end was sent');
+      return;
+    }
+    process.nextTick(gen);
+  }
+  process.nextTick(gen);
+  var readLength = 0;
+  var pendingReads = 0;
+  var ended = false;
+  var endHandled = false;
+  rwStream
+    .on('error', function (err) { done(err); })
+    .on('data', function (data) {
+      pendingReads++;
+      rwStream.pause();
+      setTimeout(function () { // simulate slow handling of data
+        readLength += data.length;
+        pendingReads--;
+        rwStream.resume();
+        if (ended) handleEnd();
+      }, 0);
+    })
+    .on('end', function () { ended = true; console.warn('on end'); handleEnd(); });
+  function handleEnd() {
+    if (endHandled) return;
+    if (!pendingReads) {
+      endHandled = true;
+      t.equal(readLength, DATA_LENGTH);
+      done();
+    }
+  }
+});
+
 test('cm.setData and cm.getDataStream save and retrieve large binary data', function (done) {
   var CHUNK_SIZE = 64 * 1024; // 64KB
   var DATA_LENGTH = 2 * 1024 * 1024 + 25; // 2,025 KB
   var shasum = crypto.createHash('sha1');
   var resultDigest;
-  var wstream = new Stream();
-  var rstream = new MemoryStream();
-  wstream.pipe(rstream);
+  var rwStream = createBufferThroughStream();
   var bytesToGenerate = DATA_LENGTH;
   function flow() {
     var size = (bytesToGenerate > CHUNK_SIZE) ? CHUNK_SIZE : bytesToGenerate;
     var buff = crypto.randomBytes(size);
     shasum.update(buff);
-    wstream.emit('data', new Buffer(buff));
+    rwStream.write(buff);
     bytesToGenerate -= size;
     if (!bytesToGenerate) {
-      wstream.emit('end');
+      rwStream.end();
+      console.warn('end was sent');
       resultDigest = shasum.digest('base64');
       return;
     }
     process.nextTick(flow);
   }
   process.nextTick(flow);
-  cm.set(KEY, rstream, 'application/octet-stream', function (err, result) {
+  cm.set(KEY, rwStream, 'application/octet-stream', function (err, result) {
     t.isNull(err);
     cm.getMeta(KEY, function (err, meta) {
       t.isNull(err);
